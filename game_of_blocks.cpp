c33cad518c30151b9365e91a7979685bb5dea62c
Siedmiu <grzesiapoczta@gmail.com>
2024-11-16 12:04:24 +0100

    TEXTURE ISSUE FIXED

--- game_of_blocks.cpp
+++ game_of_blocks.cpp
@@ -79,7 +79,7 @@ int main() {
     //OPENGL------------------------------------------------------------------------
     glEnable(GL_DEPTH_TEST);
     glEnable(GL_CULL_FACE);
-    //glEnable(GL_MULTISAMPLE);
+    glEnable(GL_MULTISAMPLE);
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
